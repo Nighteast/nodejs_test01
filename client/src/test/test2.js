20d5518a7344e3aa3662a98f2c0965c08c3b68cb
nighteast <jung946244@naver.com>
2024-05-07 14:43:39 +0900

    test link add 2

---
+++ client/src/test/test2.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function test2() {
+  return (
+    <div>test2</div>
+  )
+}
+export default test2;
---
+++ client/src/test/test3.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function test3() {
+  return (
+    <div>test3</div>
+  )
+}
+export default test3;
