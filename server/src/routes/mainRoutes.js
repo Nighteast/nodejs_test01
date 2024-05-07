6497f1e4a15ca36d3e560b9a8ca5e564e7874814
nighteast <jung946244@naver.com>
2024-05-07 15:58:19 +0900

    main router test

--- server/src/routes/mainRoutes.js
+++ server/src/routes/mainRoutes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 const routes = new Router();
 
+routes.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 routes.get('/api/message', (req, res) => {
-    res.json({ message: 'Hello from Node.js server!'});
+    res.json({ message: 'Hello from Node.js server!' });
 });
 
 routes.get('/test/message1', (req, res) => {
-  res.json({testMessage1: 'testMessage1 response'});
+    res.json({ testMessage1: 'testMessage1 response' });
 });
 
 module.exports = routes;
--- server/src/server.js
+++ server/src/server.js
@@ -9,10 +9,6 @@ app.use(express.json());
 
 app.use(mainRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
