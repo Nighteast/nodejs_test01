fca9869dcd5e6c8986136bb54653cce6f926bdfe
nighteast <jung946244@naver.com>
2024-05-07 16:10:06 +0900

    fetch test

--- client/src/test/test1.js
+++ client/src/test/test1.js
@@ -3,22 +3,27 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function Test1() {
-    const [testMessage1, setTestMessage1] = useState('');
+  const [testMessage1, setTestMessage1] = useState('');
 
-    const fetchMessage = () => {
-        axios.get('http://localhost:5000/test/message1')
-             .then(response => {
-                 setTestMessage1(response.data.testMessage1);
-             })
-             .catch(error => {
-                 console.error('Error fetching data: ', error);
-             });
+  const fetchMessage = () => {
+    if (testMessage1 !== '' && testMessage1 !== null) {
+      setTestMessage1('');
+      return;
     };
+
+    axios.get('http://localhost:5000/test/message1')
+      .then(response => {
+        setTestMessage1(response.data.testMessage1);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
+      });
+  };
 
   return (
     <div className="test1">
-        <button onClick={fetchMessage}>link1</button>
-        <div>{testMessage1}</div>
+      <button onClick={fetchMessage}>link1</button>
+      <div>{testMessage1}</div>
     </div>
   )
 }
